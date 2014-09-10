57c7858ea48d504aeb1c5c9ddb00d0cd7790b703
Julian Schuette <julian.schuette@aisec.fraunhofer.de>
2014-09-10 19:01:53 +0200

    Added Z3-str binary (unpack to /opt/) and a model for TelephonyManager

---
+++ mymodels/src/models/android/telephony/TelephonyManager.java
@@ -0,0 +1,13 @@
+package models.android.telephony;
+
+import acteve.symbolic.integer.*;
+import acteve.symbolic.string.*;
+
+public class TelephonyManager
+{
+
+    public static Expression getNetworkOperator____java_lang_String()
+    {
+    	return new acteve.symbolic.string.SymbolicString("sym_android_telephony_getNetworkOperator____java_lang_String");
+    }
+}
